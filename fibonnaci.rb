5a75dc06f839e6b98d81761e47ac798defa2db00
mosesakor <51649164+mosesakor@users.noreply.github.com>
2020-12-06 23:22:46 +0100

    Update fibonnaci.rb

--- fibonnaci.rb
+++ fibonnaci.rb
@@ -1,16 +1,11 @@
 def fib(n)
   a = 0
   b = 1
-  temp = nil
-
-  result = [0, 1]
-  while result.length < n
-    result.push(a + b)
-    temp = a
-    a = b
-    b = temp + b
+  
+  n.times do
+    a, b = b, a + b
   end
-  result
+  b
 end
 
 
